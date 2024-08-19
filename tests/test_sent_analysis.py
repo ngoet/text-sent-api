56e94b7ac3c79fc00c334e7f2170851c0c6d093f
Niels Goet <ndgoet@gmail.com>
2024-08-19 15:53:20 +0200

    test: update unit tests for compatibility with CI job

--- tests/test_sent_analysis.py
+++ tests/test_sent_analysis.py
@@ -1,13 +1,25 @@
+import os
+import sys
+import unittest
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
 from utils import analyze_sentiment
 
 
-def test_negative_sent() -> None:
-    result = analyze_sentiment("I'm having a bad day.")
-    assert (
-        result.json() == '{"status": "success", "sentiment_score": -0.6999999999999998, "error": null, "warning": null}'
-    )
+class TestSentUtils(unittest.TestCase):
+    def test_negative_sent(self) -> None:
+        result = analyze_sentiment("I'm having a bad day.")
+        self.assertEqual(
+            result.json(),
+            '{"status": "success", "sentiment_score": -0.6999999999999998, "error": null, "warning": null}',
+        )
+
+    def test_positive_sent(self) -> None:
+        result = analyze_sentiment("I'm having a great day.")
+        self.assertEqual(result.json(), '{"status": "success", "sentiment_score": 0.8, "error": null, "warning": null}')
 
 
-def test_positive_sent() -> None:
-    result = analyze_sentiment("I'm having a great day.")
-    assert result.json() == '{"status": "success", "sentiment_score": 0.8, "error": null, "warning": null}'
+if __name__ == "__main__":
+    unittest.main()
